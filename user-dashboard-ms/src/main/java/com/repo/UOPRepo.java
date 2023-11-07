84f31a64910c4be57091876617c25f5e45f7ffb9
Kartikey Vishnoi <59953234+Kartikey246@users.noreply.github.com>
2023-11-07 12:09:32 -0800

    Delete user-dashboard-ms directory

--- user-dashboard-ms/src/main/java/com/repo/UOPRepo.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.model.UserOwnedPolicy;
-
-public interface UOPRepo extends JpaRepository<UserOwnedPolicy, Long> {
-
-}
